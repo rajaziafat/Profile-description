4aaa277f816cec12fc78871f9a74421e65645077
Ali Aalam <aliaalam.me@gmail.com>
2024-03-27 23:23:55 +0500

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'md': '1024px',
+      },
+    },
   },
   plugins: [],
 }
